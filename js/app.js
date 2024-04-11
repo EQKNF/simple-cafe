3a676a869d77e86b22c247016bd106f0879c8cea
Emil Fredriksen <emil.fredriksen@hotmail.no>
2024-04-11 23:08:51 +0200

    working sidebar, add active tabs selector

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@ const navSlide = () => {
   const line1 = document.querySelector(".line1");
   const line2 = document.querySelector(".line2");
   const line3 = document.querySelector(".line3");
+  const sidebar = document.querySelector(".sidebar-container");
 
   sidebarBtn.addEventListener("click", () => {
     line1.classList.toggle("toggle-line1");
     line2.classList.toggle("toggle-line2");
     line3.classList.toggle("toggle-line3");
+
+    sidebar.classList.toggle("sidebar-toggle-active");
   });
 };
 
